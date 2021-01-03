9ccb761711f87d1aa80eaf1120a8a8798baa5929
Jcarri14 <j.carrion@students.salle.url.edu>
2021-01-03 13:38:38 +0100

    create Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+
+import controller.Controller;
+import db.DBConnector;
+import model.Model;
+import view.View;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            //DBConnector dbConnector = DBConnector.getInstance();
+
+            Model model = new Model();
+
+            View view = new View();
+
+            Controller controller = new Controller(model, view);
+            addShutDownHook();
+            controller.execute();
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addShutDownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("***************************************************");
+            System.out.println("Tancant PizziSalle ...");
+            System.out.println("***************************************************");
+
+            try {
+                //DBConnector.getInstance().disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }));
+    }
+
+}
