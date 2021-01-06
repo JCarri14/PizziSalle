f322dfafba60e5bbfa7017fb3ac04e9eeafc435d
Jcarri14 <j.carrion@students.salle.url.edu>
2021-01-06 04:14:14 +0100

    refactor DBObjectManager for dynamic model class support

--- src/main/java/db/managers/DBObjectManager.java
+++ src/main/java/db/managers/DBObjectManager.java
@@ -5,28 +5,15 @@ import db.callbacks.DBCallback;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 /* Observable */
 /* Analogy to DAO pattern */
-public abstract class DBObjectManager {
-    protected PropertyChangeSupport support;
-
-    protected DBObjectManager() {
-        this.support = new PropertyChangeSupport(this);
-    }
-
-    public void addObserver(PropertyChangeListener pcl) {
-        this.support.addPropertyChangeListener(pcl);
-    }
-
-    public void removeObserver(PropertyChangeListener pcl) {
-        this.support.removePropertyChangeListener(pcl);
-    }
-
-    public abstract void get(Map<String, String> filters, final DBCallback callback) throws SQLException;
-    public abstract void getAll(final DBCallback callback) throws SQLException;
-    public abstract void post(Object element, final DBCallback callback) throws SQLException;
-    public abstract void update(Object element, final DBCallback callback) throws SQLException;
-    public abstract void delete(int elementId, final DBCallback callback) throws SQLException;
+public abstract class DBObjectManager<Param> {
+    public abstract List<Param> get(Map<String, String> filters) throws SQLException;
+    public abstract List<Param> getAll() throws SQLException;
+    public abstract int insert(Param element) throws SQLException;
+    public abstract boolean update(Param element) throws SQLException;
+    public abstract boolean delete(Map<String, String> filters) throws SQLException;
 }
