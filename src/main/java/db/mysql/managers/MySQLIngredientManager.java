65ddd6964b463a48ee73efa025a75fa155ce1142
Jcarri14 <j.carrion@students.salle.url.edu>
2021-01-06 04:18:36 +0100

    update IngredientManager implementation

--- src/main/java/db/mysql/managers/MySQLIngredientManager.java
+++ src/main/java/db/mysql/managers/MySQLIngredientManager.java
@@ -2,15 +2,20 @@ package db.mysql.managers;
 
 import db.callbacks.DBCallback;
 import db.managers.MySQLManager;
+import db.mappers.IngredientMapper;
+import db.mappers.MassMapper;
+import db.mappers.UserMapper;
 import model.pizza.Ingredient;
+import model.pizza.Mass;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MySQLIngredientManager extends MySQLManager {
+public class MySQLIngredientManager extends MySQLManager<Ingredient> {
     private static MySQLIngredientManager instance;
 
     protected MySQLIngredientManager(Connection conn) {
@@ -29,35 +34,55 @@ public class MySQLIngredientManager extends MySQLManager {
     }
 
     @Override
-    public void get(Map<String, String> filters, DBCallback callback) throws SQLException {
+    public List<Ingredient> get(Map<String, String> filters) throws SQLException {
+        List<Ingredient> ingredients = new ArrayList<>();
+        query = "SELECT * FROM Ingredient";
+        query += getQueryFilters(filters);
+        stt = conn.createStatement();
+        rs = stt.executeQuery (query);
 
+        while(rs.next()) {
+            Ingredient p = IngredientMapper.MySQLResponseToObject(rs);
+            ingredients.add(p);
+        }
+        return ingredients;
     }
 
     @Override
-    public void getAll(DBCallback callback) throws SQLException {
+    public List<Ingredient> getAll() throws SQLException {
         List<Ingredient> ingredients = new ArrayList<>();
         query = "SELECT * FROM Ingredient";
         stt = conn.createStatement();
         rs = stt.executeQuery (query);
 
         while(rs.next()) {
-            //Ingredient i = new Ingredient(rs.getString("name"));
-            //ingredients.add(i);
+            Ingredient p = IngredientMapper.MySQLResponseToObject(rs);
+            ingredients.add(p);
+        }
+        return ingredients;
+    }
+
+    @Override
+    public int insert(Ingredient element) throws SQLException {
+        query = IngredientMapper.ObjectToMySQLQuery(element);
+        stt = conn.createStatement();
+        stt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+
+        rs = stt.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        } else {
+            throw new SQLException("KO. Creating new customer failed, no ID obtained.");
         }
     }
 
     @Override
-    public void post(Object element, DBCallback callback) {
-
+    public boolean update(Ingredient element) throws SQLException {
+        return false;
     }
 
     @Override
-    public void delete(int elementId, DBCallback callback) {
-
-    }
-
-    @Override
-    public void update(Object element, DBCallback callback) {
-
+    public boolean delete(Map<String, String> filters) throws SQLException {
+        return false;
     }
 }
