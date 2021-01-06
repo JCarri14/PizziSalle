a980d97377c0615e0ca05dfe35fb8e51c8921605
Jcarri14 <j.carrion@students.salle.url.edu>
2021-01-06 04:17:20 +0100

    update DelegationManager implementation

--- src/main/java/db/mysql/managers/MySQLDelegationManager.java
+++ src/main/java/db/mysql/managers/MySQLDelegationManager.java
@@ -2,14 +2,18 @@ package db.mysql.managers;
 
 import db.callbacks.DBCallback;
 import db.managers.MySQLManager;
+import db.mappers.DelegationMapper;
+import db.mappers.UserMapper;
+import model.delegation.Delegation;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MySQLDelegationManager extends MySQLManager {
+public class MySQLDelegationManager extends MySQLManager<Delegation> {
     private static MySQLDelegationManager instance;
 
     protected MySQLDelegationManager(Connection conn) {
@@ -28,34 +32,55 @@ public class MySQLDelegationManager extends MySQLManager {
     }
 
     @Override
-    public void get(Map<String, String> filters, DBCallback callback) throws SQLException {
+    public List<Delegation> get(Map<String, String> filters) throws SQLException {
+        List<Delegation> delegations = new ArrayList<>();
+        query = "SELECT * FROM Delegation";
+        query += getQueryFilters(filters);
+        stt = conn.createStatement();
+        rs = stt.executeQuery (query);
+
+        while(rs.next()) {
+            Delegation d = DelegationMapper.MySQLResponseToObject(rs);
+            delegations.add(d);
+        }
+        return delegations;
     }
 
     @Override
-    public void getAll(DBCallback callback) throws SQLException {
-        List<String> delegations = new ArrayList<>();
+    public List<Delegation> getAll() throws SQLException {
+        List<Delegation> delegations = new ArrayList<>();
         query = "SELECT * FROM Delegation";
         stt = conn.createStatement();
         rs = stt.executeQuery (query);
 
         while(rs.next()) {
-            String d = rs.getString("name");
+            Delegation d = DelegationMapper.MySQLResponseToObject(rs);
             delegations.add(d);
+        }
+        return delegations;
+    }
+
+    @Override
+    public int insert(Delegation element) throws SQLException {
+        query = DelegationMapper.ObjectToMySQLQuery(element);
+        stt = conn.createStatement();
+        stt.executeUpdate(query, Statement.RETURN_GENERATED_KEYS);
+
+        rs = stt.getGeneratedKeys();
+        if (rs.next()) {
+            return rs.getInt(1);
+        } else {
+            throw new SQLException("KO. Creating new customer failed, no ID obtained.");
         }
     }
 
     @Override
-    public void post(Object element, DBCallback callback) {
-
+    public boolean update(Delegation element) throws SQLException {
+        return false;
     }
 
     @Override
-    public void delete(int elementId, DBCallback callback) {
-
-    }
-
-    @Override
-    public void update(Object element, DBCallback callback) {
-
+    public boolean delete(Map<String, String> filters) throws SQLException {
+        return false;
     }
 }
